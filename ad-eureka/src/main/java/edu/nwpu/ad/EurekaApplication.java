a3af4477459ddb8e11d0554c705780328377f719
yangyuanlin <yylin7239@163.com>
2019-10-20 18:28:29 +0800

    部署多个Eureka Server

--- ad-eureka/src/main/java/edu/nwpu/ad/EurekaApplication.java
+++ ad-eureka/src/main/java/edu/nwpu/ad/EurekaApplication.java
@@ -1,5 +1,9 @@
 package edu.nwpu.ad;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 /**
  *
  */
