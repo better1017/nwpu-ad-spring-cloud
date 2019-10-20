4ef54cebed91f85750cdb0626b3d444b3b24c484
yangyuanlin <yylin7239@163.com>
2019-10-20 17:09:34 +0800

    搭建项目架构，编写并部署了单节点的Eureka Server

---
+++ ad-eureka/src/main/java/edu/nwpu/ad/EurekaApplication.java
@@ -0,0 +1,12 @@
+package edu.nwpu.ad;
+
+/**
+ *
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
