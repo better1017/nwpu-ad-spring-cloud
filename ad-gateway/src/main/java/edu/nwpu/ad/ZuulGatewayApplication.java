8b25624b2d91f249e1d558e770aeaf018e8f5be5
yangyuanlin <yylin7239@163.com>
2019-10-21 15:22:58 +0800

    网关启动程序的开发

---
+++ ad-gateway/src/main/java/edu/nwpu/ad/ZuulGatewayApplication.java
@@ -0,0 +1,17 @@
+package edu.nwpu.ad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ *
+ */
+@EnableZuulProxy
+@SpringCloudApplication
+public class ZuulGatewayApplication {
+    public static void main(String[] args) {
+
+        SpringApplication.run(ZuulGatewayApplication.class, args);
+    }
+}
