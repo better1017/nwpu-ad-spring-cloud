be10fa7a78d47b26c46e8a1fc0fc430afd375123
ylyang <yylin7239@163.com>
2019-11-07 17:39:27 +0800

    层级索引操作的实现：第二层、第三层、第四层。

--- nwpu-ad-service/ad-search/src/main/java/edu/nwpu/ad/handle/AdLevelDataHandler.java
+++ nwpu-ad-service/ad-search/src/main/java/edu/nwpu/ad/handle/AdLevelDataHandler.java
@@ -1,8 +1,27 @@
 package edu.nwpu.ad.handle;
 
+import com.alibaba.fastjson.JSON;
+import edu.nwpu.ad.dump.table.*;
+import edu.nwpu.ad.index.DataTable;
 import edu.nwpu.ad.index.IndexAware;
+import edu.nwpu.ad.index.adplan.AdPlanIndex;
+import edu.nwpu.ad.index.adplan.AdPlanObject;
+import edu.nwpu.ad.index.adunit.AdUnitIndex;
+import edu.nwpu.ad.index.adunit.AdUnitObject;
+import edu.nwpu.ad.index.creative.CreativeIndex;
+import edu.nwpu.ad.index.creative.CreativeObject;
+import edu.nwpu.ad.index.creativeunit.CreativeUnitIndex;
+import edu.nwpu.ad.index.creativeunit.CreativeUnitObject;
+import edu.nwpu.ad.index.district.UnitDistrictIndex;
+import edu.nwpu.ad.index.interest.UnitItIndex;
+import edu.nwpu.ad.index.keyword.UnitKeywordIndex;
 import edu.nwpu.ad.mysql.constant.OpType;
+import edu.nwpu.ad.utils.CommonUtils;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * 1.索引之间存在着层级的划分，也就是依赖关系的划分
@@ -12,7 +31,195 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class AdLevelDataHandler {
 
-    private static <K,V> void handleBinlogEvent(
+    private static void handleLevel2(AdPlanTable planTable, OpType type) {
+        // 把AdPlanTable转变成AdPlanObject
+        AdPlanObject planObject = new AdPlanObject(
+                planTable.getId(),
+                planTable.getUserId(),
+                planTable.getPlanStatus(),
+                planTable.getStartDate(),
+                planTable.getEndDate()
+        );
+        // 利用handleBinlogEvent写进索引
+        handleBinlogEvent(
+                DataTable.of(AdPlanIndex.class),
+                planObject.getPlanId(),
+                planObject,
+                type
+        );
+    }
+
+    public static void handleLevel2(AdCreativeTable creativeTable,
+                                    OpType type) {
+
+        CreativeObject creativeObject = new CreativeObject(
+                creativeTable.getAdId(),
+                creativeTable.getName(),
+                creativeTable.getType(),
+                creativeTable.getMaterialType(),
+                creativeTable.getHeight(),
+                creativeTable.getWidth(),
+                creativeTable.getAuditStatus(),
+                creativeTable.getAdUrl()
+        );
+        handleBinlogEvent(
+                DataTable.of(CreativeIndex.class),
+                creativeObject.getAdId(),
+                creativeObject,
+                type
+        );
+    }
+
+    public static void handleLevel3(AdUnitTable unitTable, OpType type) {
+
+        AdPlanObject adPlanObject = DataTable.of(
+                AdPlanIndex.class
+        ).get(unitTable.getPlanId());
+        if (null == adPlanObject) {
+            log.error("handleLevel3 found AdPlanObject error: {}",
+                    unitTable.getPlanId());
+            return;
+        }
+
+        AdUnitObject unitObject = new AdUnitObject(
+                unitTable.getUnitId(),
+                unitTable.getUnitStatus(),
+                unitTable.getPositionType(),
+                unitTable.getPlanId(),
+                adPlanObject
+        );
+
+        handleBinlogEvent(
+                DataTable.of(AdUnitIndex.class),
+                unitTable.getUnitId(),
+                unitObject,
+                type
+        );
+    }
+
+    public static void handleLevel3(AdCreativeUnitTable creativeUnitTable,
+                                    OpType type) {
+
+        if (type == OpType.UPDATE) {
+            log.error("CreativeUnitIndex not support update");
+            return;
+        }
+
+        AdUnitObject unitObject = DataTable.of(
+                AdUnitIndex.class
+        ).get(creativeUnitTable.getUnitId());
+        CreativeObject creativeObject = DataTable.of(
+                CreativeIndex.class
+        ).get(creativeUnitTable.getAdId());
+
+        if (null == unitObject || null == creativeObject) {
+            log.error("AdCreativeUnitTable index error: {}",
+                    JSON.toJSONString(creativeUnitTable));
+            return;
+        }
+
+        CreativeUnitObject creativeUnitObject = new CreativeUnitObject(
+                creativeUnitTable.getAdId(),
+                creativeUnitTable.getUnitId()
+        );
+        handleBinlogEvent(
+                DataTable.of(CreativeUnitIndex.class),
+                CommonUtils.stringConcat(
+                        creativeUnitObject.getAdId().toString(),
+                        creativeUnitObject.getUnitId().toString()
+                ),
+                creativeUnitObject,
+                type
+        );
+    }
+
+    public static void handleLevel4(AdUnitDistrictTable unitDistrictTable,
+                                    OpType type) {
+
+        if (type == OpType.UPDATE) {
+            log.error("district index can not support update");
+            return;
+        }
+
+        AdUnitObject unitObject = DataTable.of(
+                AdUnitIndex.class
+        ).get(unitDistrictTable.getUnitId());
+        if (unitObject == null) {
+            log.error("AdUnitDistrictTable index error: {}",
+                    unitDistrictTable.getUnitId());
+            return;
+        }
+
+        String key = CommonUtils.stringConcat(
+                unitDistrictTable.getProvince(),
+                unitDistrictTable.getCity()
+        );
+        Set<Long> value = new HashSet<>(
+                Collections.singleton(unitDistrictTable.getUnitId())
+        );
+        handleBinlogEvent(
+                DataTable.of(UnitDistrictIndex.class),
+                key, value,
+                type
+        );
+    }
+
+    public static void handleLevel4(AdUnitItTable unitItTable, OpType type) {
+
+        if (type == OpType.UPDATE) {
+            log.error("it index can not support update");
+            return;
+        }
+
+        AdUnitObject unitObject = DataTable.of(
+                AdUnitIndex.class
+        ).get(unitItTable.getUnitId());
+        if (unitObject == null) {
+            log.error("AdUnitItTable index error: {}",
+                    unitItTable.getUnitId());
+            return;
+        }
+
+        Set<Long> value = new HashSet<>(
+                Collections.singleton(unitItTable.getUnitId())
+        );
+        handleBinlogEvent(
+                DataTable.of(UnitItIndex.class),
+                unitItTable.getItTag(),
+                value,
+                type
+        );
+    }
+
+    public static void handleLevel4(AdUnitKeywordTable keywordTable,
+                                    OpType type) {
+
+        if (type == OpType.UPDATE) {
+            log.error("keyword index can not support update");
+            return;
+        }
+
+        AdUnitObject unitObject = DataTable.of(
+                AdUnitIndex.class
+        ).get(keywordTable.getUnitId());
+        if (unitObject == null) {
+            log.error("AdUnitKeywordTable index error: {}",
+                    keywordTable.getUnitId());
+            return;
+        }
+
+        Set<Long> value = new HashSet<>(
+                Collections.singleton(keywordTable.getUnitId())
+        );
+        handleBinlogEvent(
+                DataTable.of(UnitKeywordIndex.class),
+                keywordTable.getKeyword(),
+                value,
+                type
+        );
+    }
+
+    private static <K, V> void handleBinlogEvent(
             IndexAware<K, V> index,
             K key,
             V value,
--- nwpu-ad-service/ad-search/src/main/java/edu/nwpu/ad/utils/CommonUtils.java
+++ nwpu-ad-service/ad-search/src/main/java/edu/nwpu/ad/utils/CommonUtils.java
@@ -8,4 +8,15 @@ public class CommonUtils {
                                        Supplier<V> factory) {
         return map.computeIfAbsent(key, k -> factory.get());
     }
+
+    // 实现字符串之间通过连字符进行拼接
+    public static String stringConcat(String... args) {
+        StringBuilder result = new StringBuilder();
+        for (String arg : args) {
+            result.append(arg);
+            result.append("-");
+        }
+        result.deleteCharAt(result.length() - 1);
+        return result.toString();
+    }
 }
