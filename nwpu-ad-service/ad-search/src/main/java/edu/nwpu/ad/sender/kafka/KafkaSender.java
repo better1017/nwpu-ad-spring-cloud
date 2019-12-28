185027eeab4a052db282d28c995fb22d2ad072f9
ylyang <yylin7239@163.com>
2019-12-28 21:03:37 +0800

    将增量数据投递到Kafka

---
+++ nwpu-ad-service/ad-search/src/main/java/edu/nwpu/ad/sender/kafka/KafkaSender.java
@@ -0,0 +1,52 @@
+package edu.nwpu.ad.sender.kafka;
+
+import com.alibaba.fastjson.JSON;
+import edu.nwpu.ad.mysql.dto.MySqlRowData;
+import edu.nwpu.ad.sender.ISender;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+/**
+ *
+ */
+@Component("kafkaSender")
+public class KafkaSender implements ISender {
+
+    @Value("${adconf.kafka.topic}")
+    private String topic;
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    @Autowired
+    public KafkaSender(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    @Override
+    public void sender(MySqlRowData rowData) {
+
+        kafkaTemplate.send(
+                topic, JSON.toJSONString(rowData)
+        );
+    }
+
+    @KafkaListener(topics = {"ad-search-mysql-data"}, groupId = "ad-search")
+    public void processMysqlRowData(ConsumerRecord<?, ?> record) {
+        Optional<?> kafkaMessage = Optional.ofNullable(record.value());
+        if (kafkaMessage.isPresent()) {
+            Object message = kafkaMessage.get();
+            MySqlRowData rowData = JSON.parseObject(
+                    message.toString(),
+                    MySqlRowData.class
+            );
+            System.out.println("kafka processMysqlRowData: " +
+                    JSON.toJSONString(rowData));
+        }
+    }
+}
